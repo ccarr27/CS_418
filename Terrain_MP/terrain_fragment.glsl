2cbfe0b8c697a7b91101e2c8c9dc23aea1f8d80e
Carlos Carretero <carlosrandom227@gmail.com>
2024-10-24 19:50:21 -0500

    terrain mp commit

---
+++ Terrain_MP/terrain_fragment.glsl
@@ -0,0 +1,19 @@
+#version 300 es
+precision highp float;
+
+
+
+uniform vec3 lightDir;
+uniform vec4 groundColor;
+uniform vec3 lightcolor;
+uniform vec3 halfway;
+
+in vec3 vnormal;
+out vec4 color;
+
+
+void main() {
+    float blinn = pow(max(dot(normalize(vnormal), halfway), 0.0), 100.0);
+    float lambert = max(dot(normalize(vnormal), lightDir), 0.0);
+    color = vec4(groundColor.rgb * (lambert * lightcolor) + (vec3(1,1, 1) * blinn), 1.0);
+}
